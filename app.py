824d4ad773304b80c021736cc489b07dec5dfc17
Sergio Lucero <sergiolucero@gmail.com>
2022-08-04 21:58:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st, pandas as pd
 import folium
 
 st.header('Calles via OpenStreetMaps')
-
+st.image('colon8000.png')
 st.header('Mapa del Norte (con Folium)')
-
+st.image('colonNorte.png')
